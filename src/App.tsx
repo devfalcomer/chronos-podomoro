0ff8c09006584c9d5f15c75b8db7a02457388ce7
devfalcomer <falcomerart@gmail.com>
2025-12-24 19:17:21 -0300

    Importando e usando arquivos de CSS modules com escopo de componente

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import { Heading } from './components/Heading';
+
 import './styles/theme.css';
 import './styles/global.css';
 
 export function App() {
   return (
     <>
-      <h1>Olá Mundo!</h1>
+      <Heading/>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae vel
         voluptatem, quidem velit iste quisquam, ipsa illum modi officia
