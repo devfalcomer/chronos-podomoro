4962c94cd495aa64ac7b527c8c8dafc4f841984a
devfalcomer <falcomerart@gmail.com>
2025-12-24 21:36:30 -0300

    Criando a estrutura HTML e CSS do app para cada seção do site

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,40 @@
-import { Heading } from './components/Heading';
-
 import './styles/theme.css';
 import './styles/global.css';
-import { TimerIcon } from 'lucide-react';
 
 export function App() {
   return (
     <>
-      <Heading>
-        Olá Mundo 1
-        <button> <TimerIcon/> </button>
-      </Heading>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae vel
-        voluptatem, quidem velit iste quisquam, ipsa illum modi officia
-        provident molestias minus sit nihil, qui libero dolorem dicta rem
-        accusamus!
-      </p>
+      <div className='container'>
+        <div className='content'>
+          <section>
+            LOGO
+          </section>
+        </div>
+      </div>
+
+      <div className='container'>
+        <div className='content'>
+          <section>
+            MENU
+          </section>
+        </div>
+      </div>
+
+      <div className='container'>
+        <div className='content'>
+          <section>
+            FORM
+          </section>
+        </div>
+      </div>
+
+      <div className='container'>
+        <div className='content'>
+          <section>
+            FOOTER
+          </section>
+        </div>
+      </div>
     </>
   );
 }
