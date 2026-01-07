1ec93964dcfef51b321250016c0609ff0ba3adb0
devfalcomer <falcomerart@gmail.com>
2026-01-07 14:33:15 -0300

    Criando página home e umm template para todas as outras páginas

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,23 @@
+import '../../styles/global.css';
+import '../../styles/theme.css';
+
+
+import { Container } from '../../components/Container';
+import { CountDown } from '../../components/CountDown';
+import { MainTemplate } from '../../templates/MainTemplate';
+import { MainForm } from '../../components/MainForm';
+
+export function Home() {
+  return (
+    <>
+      <MainTemplate>
+        <Container>
+          <CountDown />
+        </Container>
+        <Container>
+          <MainForm />
+        </Container>
+      </MainTemplate>
+    </>
+  );
+}
