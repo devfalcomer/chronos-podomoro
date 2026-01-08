37bba368e3f9560519d5fce7917fdfb3f094978a
devfalcomer <falcomerart@gmail.com>
2026-01-08 15:33:02 -0300

    Estado global da aplicação com useState

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -6,14 +6,23 @@ import { Container } from '../../components/Container';
 import { CountDown } from '../../components/CountDown';
 import { MainTemplate } from '../../templates/MainTemplate';
 import { MainForm } from '../../components/MainForm';
+import type { TaskStateModel } from '../../models/TaskStateModel';
 
-export function Home() {
+type HomeProps = {
+  state: TaskStateModel,
+  setState: React.Dispatch<React.SetStateAction<TaskStateModel>>
+}
+
+export function Home(props: HomeProps) {
+  console.log(props);
+  
   return (
     <>
       <MainTemplate>
         <Container>
           <CountDown />
         </Container>
+        
         <Container>
           <MainForm />
         </Container>
